48c3da996437d0ee89d6e908ff64823a22d20866
Tumri <tumri@live.com>
2015-07-13 01:38:21 -0400

    Reset Repository

--- app/models/article.rb
+++
@@ -1,7 +0,0 @@
-class Article < ActiveRecord::Base
-  belongs_to :user
-  default_scope -> { order(created_at: :desc) }
-  validates :user_id, presence: true
-  validates :title, presence: true
-  validates :body, presence: true
-end
