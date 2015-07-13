074897cd48e091fd56e7b3f7743ccbfde560c6b4
Tumri <tumri@live.com>
2015-07-13 03:47:46 -0400

    Add moonacolor theme to CKEditor.

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -8,7 +8,7 @@ CKEDITOR.editorConfig = function( config )
   // Define changes to default configuration here. For example:
   // config.language = 'fr';
   // config.uiColor = '#AADC6E';
-  config.skin = 'office2013';
+  config.skin = 'moonocolor';
 
   /* Filebrowser routes */
   // The location of an external file browser, that should be launched when "Browse Server" button is pressed.
